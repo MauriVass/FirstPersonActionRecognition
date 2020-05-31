76c0b8b576814af0abba91a5145c8e3148c4fad5
Eddy <Jikeh@users.noreply.github.com>
2020-05-31 11:14:11 +0200

    Added possibility to skip CAM during training

---
+++ objectAttentionModelConvLSTM.py
@@ -0,0 +1,49 @@
+import torch
+import resnetMod
+import torch.nn as nn
+from torch.nn import functional as F
+from torch.autograd import Variable
+from MyConvLSTMCell import *
+
+
+class attentionModel(nn.Module):
+    def __init__(self, num_classes=61, mem_size=512, cam=True):
+        super(attentionModel, self).__init__()
+        self.num_classes = num_classes
+        self.resNet = resnetMod.resnet34(True, True)
+        self.mem_size = mem_size
+        self.weight_softmax = self.resNet.fc.weight
+        self.lstm_cell = MyConvLSTMCell(512, mem_size)
+        self.avgpool = nn.AvgPool2d(7)
+        self.dropout = nn.Dropout(0.7)
+        self.fc = nn.Linear(mem_size, self.num_classes)
+        self.classifier = nn.Sequential(self.dropout, self.fc)
+        self.compute_cam = cam
+        #MS branch
+        self.ms_model = nn.Sequential(nn.Conv2d(512, 100, 1), nn.AvgPool2d(7, stride=1), nn.Linear(512, 49), nn.Softmax(dim=1))
+
+    def forward(self, inputVariable):
+        state = (Variable(torch.zeros((inputVariable.size(1), self.mem_size, 7, 7)).cuda()),
+                 Variable(torch.zeros((inputVariable.size(1), self.mem_size, 7, 7)).cuda()))
+
+        #For MS
+        MS = []
+        for t in range(inputVariable.size(0)):
+            logit, feature_conv, feature_convNBN, ms = self.resNet(inputVariable[t])
+            MS.append(self.ms_model(ms))
+            if self.compute_cam:
+                bz, nc, h, w = feature_conv.size()
+                feature_conv1 = feature_conv.view(bz, nc, h*w)
+                probs, idxs = logit.sort(1, True)
+                class_idx = idxs[:, 0]
+                cam = torch.bmm(self.weight_softmax[class_idx].unsqueeze(1), feature_conv1)
+                attentionMAP = F.softmax(cam.squeeze(1), dim=1)
+                attentionMAP = attentionMAP.view(attentionMAP.size(0), 1, 7, 7)
+                attentionFeat = feature_convNBN * attentionMAP.expand_as(feature_conv)
+                state = self.lstm_cell(attentionFeat, state)
+            else:
+                state = self.lstm_cell(feature_convNBN, state)
+        feats1 = self.avgpool(state[1]).view(state[1].size(0), -1)
+        feats = self.classifier(feats1)
+
+        return feats, feats1, MS

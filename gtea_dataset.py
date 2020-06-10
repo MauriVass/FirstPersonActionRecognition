133513e0fc5cfc566133aca5ca46c95e82d26c18
carlopeluso <cpelusoprivato@gmail.com>
2020-06-10 17:23:42 +0200

    define frame sampler

--- gtea_dataset.py
+++ gtea_dataset.py
@@ -137,6 +137,9 @@ class GTEA61_RGB(GTEA61):
         # such callback is to generate indices corresponding to the frames to be sampled
         if frame_sampler is None:
             self.frame_sampler = uniform_frame_sampler
+        else:
+            self.frame_sampler = frame_sampler
+
         self.video_paths = []  # holds a path for each video
         self.build_metadata(RGB_DIR, self.video_paths)
 
@@ -168,6 +171,9 @@ class GTEA61Flow(GTEA61):
         self.split = split
         if frame_sampler is None:
             self.frame_sampler = sequential_frame_sampler
+        else:
+            self.frame_sampler = frame_sampler
+
         if self.split == "train":
             self.starting_seq = "random"
         else:
@@ -250,6 +256,9 @@ class GTEA61_MS(GTEA61):
         # such callback is to generate indices corresponding to the frames to be sampled
         if frame_sampler is None:
             self.frame_sampler = uniform_frame_sampler
+        else:
+            self.frame_sampler = frame_sampler
+
         self.video_paths = []  # holds a path for each video
         self.build_metadata(RGB_DIR, self.video_paths)
         self.transform_ms = transform_ms

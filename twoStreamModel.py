e11fc1853b8f9be56e9ac264780287006b38e6c3
MauriVass <maurizio140333@gmail.com>
2020-06-15 22:10:58 +0200

    Added .py to compute CAM

--- twoStreamModel.py
+++ twoStreamModel.py
@@ -5,13 +5,13 @@ import torch.nn as nn
 
 
 class twoStreamAttentionModel(nn.Module):
-    def __init__(self, flow_model_path=None, rgb_model_path=None, seq_len_flow=5, mem_size=512, num_classes=61, join_method="average"):
+    def __init__(self, flow_model_path=None, rgb_model_path=None, seq_len_flow=5, mem_size=512, num_classes=61, join_method="average", ordering=False):
         super(twoStreamAttentionModel, self).__init__()
         self.join_method = join_method
         self.flow_model = flow_resnet34(False, channels=2*seq_len_flow, num_classes=num_classes)
         if flow_model_path is not None:
             self.flow_model.load_state_dict(torch.load(flow_model_path))
-        self.rgb_model = attentionModel(num_classes, mem_size)
+        self.rgb_model = attentionModel(num_classes, mem_size,ordering=ordering)
         if rgb_model_path is not None:
             self.rgb_model.load_state_dict(torch.load(rgb_model_path))
         self.fc2 = nn.Linear(512 * 2, num_classes, bias=True)
